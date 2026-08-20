2c89b88e086f9ba29efeb20ee013140f5c06bb78
dilaramuhtar <dilaramuhtaruse@gmail.com>
2026-08-20 19:30:38 -0400

    cucumber fix

--- cucumber.js
+++ cucumber.js
@@ -1,6 +1,5 @@
 module.exports = {
   default: {
-    paths: ["features/**/*.feature"],
     requireModule: ["ts-node/register"],
     require: ["utilities/world.ts", "utilities/hooks.ts", "steps/**/*.ts"],
     format: ["progress-bar", "summary", "allure-cucumberjs/reporter"],
